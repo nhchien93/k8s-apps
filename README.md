ddff2383b9e5cc1d661d477cd9683aa103967732
ChienNguyen <chien_nguyen@MacBook-Pro-cua-ChienNguyen.local>
2025-10-18 13:38:00 +0700

    update app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # k8s-apps
+
+## Usage
+
+```sh
+```
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello App"
 
 
 @app.route("/hello/<name>")
