9432c3cd4edc99098bb521df8c57ee2e78fc5d84
nhchien93 <nhchienqn@gmail.com>
2026-01-07 16:50:16 +0700

    Add ask function

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
+import os
+
 from flask import Flask, jsonify, request
+
+import ask_gemini
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Welcome To Home Page"
+    return 'Welcome To Home Page'
 
 
-@app.route("/hello/<name>")
+@app.route('/hello/<name>')
 def hello(name):
-    return jsonify(message=f"Welcome {name}!")
+    return jsonify(message=f'Welcome {name}!')
 
 
-@app.route("/echo", methods=["POST"])
+@app.route('/echo', methods=['POST'])
 def echo():
     data = request.get_json()
     return jsonify(received=data)
 
 
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.get_json()
+    question = data['question']
+    gemini_response = ask_gemini.ask(question)
+
+    response = {
+        "question": question,
+        "answer": gemini_response.text,
+        "token_count": {
+            "prompt_tokens": gemini_response.usage_metadata.prompt_token_count,
+            "total_tokens": gemini_response.usage_metadata.total_token_count
+        }
+    }
+
+    return jsonify(response)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=os.getenv('APP_PORT'), debug=True)
